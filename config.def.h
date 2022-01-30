68d7e7ccd11eb78225a726a51d3604a229b3c473
ThunderThief <thunderthief5@gmail.com>
2022-01-30 08:05:15 +0530

    changed font to Source Code Pro

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FantasqueSansMono Nerd Font Mono:size=12:antialias=true:autohint=true";
+static char *font = "Source Code Pro:size=10:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
